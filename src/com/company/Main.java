8a19fd227d94c2088b595965cf394f1ba514568e
Атабек Досбаев <108028306+atabekdosbaev07@users.noreply.github.com>
2022-07-11 11:40:01 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
 
-        Class clas = new Class(1,"slsd",new int[]{1,2,34,4,5,6});
+        Class clas = new Class(1,"Hello World!",new int[]{1,2,34,4,5,6});
 
         clas.getMassiveuuu(clas.getMassive());
         System.out.println("\n"+clas.getNumber());
